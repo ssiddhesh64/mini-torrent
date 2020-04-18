7c6151f563d8bce26d8a7ecca118ee1a5a0b33fa
Siddhesh Sawant <sid@Macbook-Pro.local>
2020-04-19 02:04:19 +0530

    Improvements

--- client.cpp
+++ client.cpp
@@ -1,3 +1,10 @@
+/* This program takes 2 arguments
+    1. Port Number of server which is running
+    2. IP address of server which is running
+    run as ./client portno ip_address
+*/
+
+
 #include<stdio.h>
 #include<unistd.h>
 #include<arpa/inet.h>
@@ -6,7 +13,8 @@
 #include<netinet/in.h>
 #include<stdlib.h>
 #include<string> 
-// #define port 8000
+#include<netdb.h>
+
 
 using namespace std;
 
@@ -15,7 +23,7 @@ int main(int argc, char** argv){
     int port = stoi(argv[1]);
     int socket_fd, val_read;
     struct sockaddr_in serv_addr;
-    
+    struct hostent *server;
     char buffer[1024] = {0};
     //char msg[1024];
     string msg;
@@ -24,18 +32,27 @@ int main(int argc, char** argv){
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
+
+    server = gethostbyname(argv[2]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
     memset(&serv_addr, '0', sizeof(serv_addr));
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
     // serv_addr.sin_addr.s_addr = INADDR_ANY;
-    inet_pton(socket_fd, argv[2], &serv_addr.sin_addr.s_addr);
+    // inet_pton(socket_fd, argv[2], &serv_addr.sin_addr.s_addr);
+    bcopy((char *)server->h_addr_list[0], (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+
+    cout << "IP address: " << serv_addr.sin_addr.s_addr << endl;
 
     if(connect(socket_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
         perror("accept failed");
         exit(EXIT_FAILURE);
     }
-    
+    cout << "Connected" << endl;
     while(1){
 
         memset(buffer, 0, sizeof buffer);  // Clears previous contents
@@ -50,6 +67,7 @@ int main(int argc, char** argv){
         printf("Server: %s\n", buffer);
     } 
 
+    shutdown(socket_fd, SHUT_RDWR);
     close(socket_fd);
 
     return 0;
--- server.cpp
+++ server.cpp
@@ -1,3 +1,8 @@
+/* This program takes 1 argument
+    1. Port no
+    run as ./server portno
+*/
+
 #include<stdio.h>
 #include<unistd.h>
 #include<bits/stdc++.h>
@@ -6,19 +11,15 @@
 #include<stdlib.h>
 #include<string>
 
-// #define port 8000
 using namespace std;
 
 int main(int argc, char** argv){
 
-    // string p(argv[1]);
-    // cout << p << endl;
     int port = stoi(argv[1]);
     int socket_fd, new_fd, val_read;
     struct sockaddr_in serv_address, cli_address;
     
     char buffer[1024] = {0};
-    //char msg[1024];
     string msg;
 
     if((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
@@ -35,32 +36,34 @@ int main(int argc, char** argv){
         exit(EXIT_FAILURE);
     }
 
-    
     if((listen(socket_fd, 5))<0){
-        perror("bind failed");
+        perror("Listening failed");
         exit(EXIT_FAILURE);
     }
 
-    cout << "listening" << endl;
+    cout << "Listening" << endl;
     if((new_fd = accept(socket_fd, (struct sockaddr *) &cli_address, (socklen_t*)&cli_address)) < 0){
         perror("Accept failed");
         exit(EXIT_FAILURE);
     }
     
-    cout << "accepted connection" << endl;
+    cout << "Connection accepted" << endl;
     while(1){
         val_read = read(new_fd, buffer, 1024);
         if(strcmp(buffer, "bye")==0) break;
         printf("Client: %s\n", buffer);
 
-        memset(buffer, 0, sizeof buffer); // Clears previous contents
+        memset(buffer, 0, sizeof buffer);               // Clears previous buffer contents
 
-        //msg.clear();     
         cout << "Server: ";
         getline(cin, msg);
         send(new_fd, msg.c_str(), msg.length(), 0);
             
     }
+    
+    shutdown(socket_fd, SHUT_RDWR);
     close(socket_fd);
+    shutdown(new_fd, SHUT_RDWR);
+    close(new_fd);
     return 0;
 }
